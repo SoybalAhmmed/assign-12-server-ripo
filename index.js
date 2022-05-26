dea37d42b66e662ad35bd1d0511168cfe663cff9
SoybalAhmmed <soybalahmed@gmail.com>
2022-05-27 04:12:10 +0600

    services load from db

--- index.js
+++ index.js
@@ -16,7 +16,14 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     try{
         await client.connect();
-        console.log("dv connected")
+        const serviceCollection = client.db('books_portal12').collection('services');
+
+        app.get('/service', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
 
 
     }
