353bc0e9498600a262e72f11209173bfa47a2588
SoybalAhmmed <soybalahmed@gmail.com>
2022-05-28 21:49:56 +0600

    delete button added

--- index.js
+++ index.js
@@ -37,6 +37,18 @@ async function run(){
         const userCollection = client.db('books_portal12').collection('users');
         const bookCollection = client.db('books_portal12').collection('books');
 
+        const verifyAdmin = async (req, res, next) => {
+          const requester = req.decoded.email;
+          const requesterAccount = await userCollection.findOne({ email: requester });
+          if (requesterAccount.role === 'admin') {
+            next();
+          }
+          else {
+            res.status(403).send({ message: 'forbidden' });
+          }
+        }
+
+
         app.get('/service', async(req, res) =>{
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -84,21 +96,16 @@ async function run(){
         res.send({admin: isAdmin})
       })
 
-      app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      app.put('/user/admin/:email', verifyJWT,verifyAdmin, async (req, res) => {
         const email = req.params.email;
-        const requester = req.decoded.email;
-        const requesterAccount = await userCollection.findOne({ email: requester });
-        if (requesterAccount.role === 'admin') {
+       
           const filter = { email: email };
           const updateDoc = {
             $set: { role: 'admin' },
           };
           const result = await userCollection.updateOne(filter, updateDoc);
           res.send(result);
-        }
-        else{
-          res.status(403).send({message: 'forbidden'});
-        }
+        
   
       })
 
@@ -115,10 +122,22 @@ async function run(){
         res.send({ result, token });
       })
 
+      app.get('/book', verifyJWT, verifyAdmin, async(req, res) =>{
+        const books = await bookCollection.find().toArray();
+        res.send(books);
+      })
+
       app.post('/book', verifyJWT, verifyAdmin, async (req, res) => {
         const book = req.body;
         const result = await bookCollection.insertOne(book);
         res.send(result);
+      });
+
+      app.delete('/book/:email', verifyJWT, verifyAdmin, async (req, res) => {
+        const email = req.params.email;
+      const filter = {email: email};
+      const result = await bookCollection.deleteOne(filter);
+      res.send(result);
       });
 
 
